70a87dbdad47cbdf2c942b7a0cd7acf24d8da742
Oskari Silvoniemi <silvoniemioskari@gmail.com>
2024-09-06 20:54:58 +0300

    configuring main components

---
+++ src/AnswerBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AnswerBar = () => {
+  return (
+    <div className="">
+      <form action="">
+        <input type="text" placeholder="_ _ _ _ _ _" />
+      </form>
+    </div>
+  );
+};
+
+export default AnswerBar;
---
+++ src/Start.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Start = () => {
+  return (
+    <div>
+      <h1>KARTTAPELI</h1>
+      <button className="border-8">PELAA PELIÄ</button>
+    </div>
+  );
+};
+
+export default Start;
