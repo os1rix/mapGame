cba63c32cc4e77e073fc5cfdb21c06bddba871c9
os1rix <silvoniemioskari@gmail.com>
2024-12-26 23:04:20 +0200

    slight design changes

--- src/Start.tsx
+++ src/Start.tsx
@@ -3,12 +3,12 @@ import React from "react"
 const Start = ({ onStartClick }: { onStartClick: () => void }) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4">KARTTAPELI</h1>
+      <h1 className="text-4xl font-bold mb-4">MAP GAME</h1>
       <button
         className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300"
         onClick={onStartClick}
       >
-        PELAA PELIÄ
+        PLAY THE GAME
       </button>
     </div>
   )
